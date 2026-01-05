928bb468e0e884961ffb2cd51f250e39155680a8
Eimmy Valeria Ochoa Morán <eimvocho@espol.edu.ec>
2026-01-05 06:53:24 -0500

    Profile

--- grimoire-app/app/(drawer)/perfil/index.js
+++ grimoire-app/app/(drawer)/perfil/index.js
@@ -1,13 +1,52 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { useState, useCallback } from 'react';
+import { View, Text, StyleSheet, Alert, ActivityIndicator } from 'react-native';
+import { supabase } from '../../../lib/supabase';
+import { useFocusEffect, useRouter } from 'expo-router';
 
-export default function Perfil() {
+export default function Personajes() {
+  const [profile, setProfile] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchProfile();
+    }, [])
+  );
+
+  async function fetchProfile() {
+    try {
+      const { data: { user } } = await supabase.auth.getUser();
+      if (!user) return;
+
+      const { data, error } = await supabase
+        .from('profile')
+        .select(`*`)
+        .eq('id', user.id)
+        .single();
+
+      if (error) throw error;
+
+      setProfile(data);
+    } catch (error) {
+      Alert.alert('Error', error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Ionicons name="construct" size={80} color="#A3B18A" />
-      <Text style={styles.text}>Perfil del Aventurero</Text>
-      <Text style={styles.subtext}>Working on it... 🧙‍♂️</Text>
+      {loading ? (
+        <ActivityIndicator style={{ marginTop: 50 }} size="large" color="#5D8C5D" />
+      ) : profile ? (
+        <View>
+          <Text style={styles.text}>Perfil de Usuario</Text>
+          <Text style={styles.subtext}>UserName: { profile.id } </Text>
+          <Text style={styles.subtext}>Avatar URL: { profile.avatar_url } </Text>
+        </View>
+      ) : (
+        <Text style={styles.text}>No se encontró el perfil.</Text>
+      )}
     </View>
   );
 }
